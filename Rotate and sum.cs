ad23e871cec25176938c86f44ae24ccece4b1542
YanaNeykova <32368269+YanaNeykova@users.noreply.github.com>
2017-11-05 00:21:04 +0200

    Add files via upload

---
+++ Rotate and sum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] inputArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int rotations = int.Parse(Console.ReadLine());
+
+        int[] tempArray = new int[inputArray.Length - 1];
+        int[] newArray = new int[inputArray.Length];
+        int[] sum = new int[inputArray.Length];
+
+        for (int rotate = 0; rotate < rotations; rotate++)
+        {
+            for (int i = 0; i < tempArray.Length; i++)
+                tempArray[i] = inputArray[i];
+
+            newArray[0] = inputArray[inputArray.Length - 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+                newArray[i + 1] = tempArray[i];
+
+            inputArray = newArray;
+
+            for (int i = 0; i < inputArray.Length; i++)
+                sum[i] += newArray[i];
+        }
+
+        Console.WriteLine(string.Join(" ", sum));
+    }
+}
---
+++ T10. Pairs by Difference.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T10_PairsByDifference
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int difference = int.Parse(Console.ReadLine());
+            int pairs = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                int temporary = array[i];
+                for (int j = 0; j < array.Length; j++)
+                {
+                    int newTemporary = array[j];
+                    if (newTemporary - temporary == difference)
+                    {
+                        pairs++;
+                    }
+                }
+            }
+
+            Console.WriteLine(pairs);
+        }
+    }
+}
